9591b186a99c2c90d1d7aae9e3a22f5ef86fd26c
mdurrant <mdurrant@expressmedia.com>
2015-11-05 06:31:28 -0700

    clean up search & add language selector (hidden for now)

--- src/app/component/layout/controller.js
+++ src/app/component/layout/controller.js
@@ -3,12 +3,32 @@ var app = angular.module('app');
 app.controller('layoutCtrl', [
     '$scope',
     '$state',
+    '$stateParams',
     '$localStorage',
     'gettext',
+    'gettextCatalog',
 
-    function ($scope, $state, $localStorage, gettext) {
+    function ($scope, $state, $stateParams, $localStorage, gettext, gettextCatalog) {
         $scope.placeholder = $localStorage.searchPlaceholder ?
             $localStorage.searchPlaceholder : gettext('Search for a Guardian...');
+
+        $scope.languages = {
+            de: 'Deutsch',
+            en: 'English',
+            es: 'Español',
+            fr: 'Français',
+            it: 'Italiano',
+            'pt-br': 'Português (Brasil)',
+            ja: '日本語'
+        };
+
+        $scope.currentLanguage = function() {
+            return gettextCatalog.getCurrentLanguage();
+        }
+
+        $scope.changeLanguage = function(lang) {
+            $state.go($state.current.name, _.extend($stateParams, {locale: lang}));
+        };
 
         $scope.searchForPlayer = function(name) {
             if (!name || name.trim().length == 0) {
