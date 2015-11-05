3ba879b0898597fabbf2112374c7f8196dfef6df
Kyle Spraggs <theman@spiffyjr.me>
2015-11-04 20:38:03 -0600

    Remember last search and react to enter

--- src/app/component/layout/controller.js
+++ src/app/component/layout/controller.js
@@ -3,13 +3,23 @@ var app = angular.module('app');
 app.controller('layoutCtrl', [
     '$scope',
     '$state',
+    '$localStorage',
+    'gettext',
 
-    function ($scope, $state) {
+    function ($scope, $state, $localStorage, gettext) {
+        $scope.placeholder = $localStorage.searchPlaceholder ?
+            $localStorage.searchPlaceholder : gettext('Search for a Guardian...');
+
         $scope.searchForPlayer = function(name) {
             if (!name || name.trim().length == 0) {
-                return;
+                if (!$localStorage.searchPlaceholder) {
+                    return
+                }
+                name = $localStorage.searchPlaceholder;
             }
 
+            $localStorage.searchPlaceholder = name;
+            $scope.name = '';
             $state.go('app.search', { name: name });
         };
     }
