b7331de00bee51a7bf39615d0b6c9531baa19b31
Kyle Spraggs <theman@spiffyjr.me>
2015-11-11 12:26:38 -0600

    Removed deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-rm -r build/
-gulp build --prod
-ssh deploy@web01.guardian.gg mkdir /tmp/ggg-release
-scp -rp build/* deploy@web01.guardian.gg:/tmp/ggg-release
-ssh deploy@web01.guardian.gg 'rm -rf /var/www/guardian.gg/* && mv /tmp/ggg-release/* /var/www/guardian.gg/ && rm -r /tmp/ggg-release'
