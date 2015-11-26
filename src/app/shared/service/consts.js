f07404f5a47e2c3873d2f2be42505ca49ead30c6
Kyle Spraggs <theman@spiffyjr.me>
2015-11-25 19:53:27 -0600

    Added zone control

--- src/app/shared/service/consts.js
+++ src/app/shared/service/consts.js
@@ -113,6 +113,7 @@ app.service('consts', [
                 13: gettextCatalog.getString('Rumble'),
                 23: gettextCatalog.getString('Elimination'),
                 15: gettextCatalog.getString('Doubles'),
+                28: gettextCatalog.getString('Zone Control'),
                 19: gettextCatalog.getString('Iron Banner'),
                 14: gettextCatalog.getString('Trials of Osiris')
             },
