616cd30195021c3c3a62bfbe1f2f39132f34cfdc
Kyle Spraggs <theman@spiffyjr.me>
2015-12-15 22:33:34 -0600

    Fixed locale switching on FireFox

--- src/app/app.js
+++ src/app/app.js
@@ -55,6 +55,9 @@ app
             var regex = new RegExp("^\/(" + Object.keys(consts.languages).join('|') + ")\/");
             var match = $location.path().match(regex);
 
+            console.log(match);
+            console.log(locale);
+
             if (!match) {
                 $log.debug('no url locale set, redirecting to: ' + locale);
                 $state.go('app.home', { locale: locale });
